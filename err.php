0a17dd5a1df232f1c3514c56281e3c4a9b73dbfc
apapacy <apapacy@gmail.com>
2013-07-04 01:17:52 +0300

    remove phpN

--- err.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo $db = pg_pconnect("host=localhost dbname=test user=Administrator ");
-echo $ex = pg_query($db, "select * from products order by name");
-$ar = pg_fetch_all($ex);
-print_r($ar);
-?>
